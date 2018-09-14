626098bfaf858009fff9807e02edfbbb72461369
harshitk2k <harshitk2k@gmail.com>
2018-09-14 21:36:42 +0530

    Implement Update Question

---
+++ app/src/main/java/com/imad/quickclassquiz/activities/EditQuestion.java
@@ -0,0 +1,204 @@
+package com.imad.quickclassquiz.activities;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.imad.quickclassquiz.R;
+import com.imad.quickclassquiz.dataModel.Question;
+
+import java.util.UUID;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class EditQuestion extends AppCompatActivity {
+
+    @BindView(R.id.questionEditText)
+    EditText title;
+
+    @BindView(R.id.option1EditText)
+    EditText option1EditText;
+
+    @BindView(R.id.option2EditText)
+    EditText option2EditText;
+
+    @BindView(R.id.option3EditText)
+    EditText option3EditText;
+
+    @BindView(R.id.option4EditText)
+    EditText option4EditText;
+
+    @BindView(R.id.option1Radio)
+    RadioButton option1radio;
+
+    @BindView(R.id.option2Radio)
+    RadioButton option2radio;
+
+    @BindView(R.id.option3Radio)
+    RadioButton option3radio;
+
+    @BindView(R.id.option4Radio)
+    RadioButton option4radio;
+
+    @BindView(R.id.saveQuestion)
+    FloatingActionButton saveQuestion;
+
+    @BindView(R.id.addQuestionToolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.option1InputLayout)
+    TextInputLayout inputLayout1;
+
+    @BindView(R.id.option2InputLayout)
+    TextInputLayout inputLayout2;
+
+    @BindView(R.id.option3InputLayout)
+    TextInputLayout inputLayout3;
+
+    @BindView(R.id.option4InputLayout)
+    TextInputLayout inputLayout4;
+
+    Question question;
+    private String correctAnswer;
+    ProgressDialog progressDialog;
+    String testId;
+    String questionId;
+    FirebaseFirestore firestore;
+    String url = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_question);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle("Update a question");
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        Intent intent = getIntent();
+        if(intent != null){
+            question = intent.getParcelableExtra("Question");
+            testId = question.getTestId();
+            questionId = question.getQuestionId();
+            url = String.format("tests/%s/questions", testId);
+            title.setText(question.getQuestion());
+            option1EditText.setText(question.getOption1());
+            option2EditText.setText(question.getOption2());
+            option3EditText.setText(question.getOption3());
+            option4EditText.setText(question.getOption4());
+            if(question.getOption1().equals(question.getCorrectOption())){
+                option1radio.setChecked(true);
+            }
+            else if(question.getOption2().equals(question.getCorrectOption())){
+                option2radio.setChecked(true);
+            }
+            else if(question.getOption3().equals(question.getCorrectOption())){
+                option3radio.setChecked(true);
+            }
+            else {
+                option4radio.setChecked(true);
+            }
+
+        }
+        progressDialog = new ProgressDialog(this);
+        firestore = FirebaseFirestore.getInstance();
+        saveQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateQuestion();
+            }
+        });
+    }
+
+    private void updateQuestion(){
+        if(title.getText().length() == 0){
+            title.requestFocus();
+            title.setError("Can't be empty");
+            return;
+        }
+        if(option1EditText.getText().length() == 0){
+            option1EditText.requestFocus();
+            inputLayout1.setError("Can't be empty");
+            return;
+        }
+        if(option2EditText.getText().length() == 0){
+            option2EditText.requestFocus();
+            inputLayout2.setError("Can't be empty");
+            return;
+        }
+        if(option3EditText.getText().length() == 0){
+            option3EditText.requestFocus();
+            inputLayout3.setError("Can't be empty");
+            return;
+        }
+        if(option4EditText.getText().length() == 0){
+            option4EditText.requestFocus();
+            inputLayout4.setError("Can't be empty");
+            return;
+        }
+        String question = title.getText().toString().trim();
+        String option1 = option1EditText.getText().toString().trim();
+        String option2 = option2EditText.getText().toString().trim();
+        String option3 = option3EditText.getText().toString().trim();
+        String option4 = option4EditText.getText().toString().trim();
+
+        if (option1radio.isChecked()) {
+            correctAnswer = option1;
+        } else if (option2radio.isChecked()) {
+            correctAnswer = option2;
+        } else if (option3radio.isChecked()) {
+            correctAnswer = option3;
+        } else if (option4radio.isChecked()) {
+            correctAnswer = option4;
+        }
+
+        if (correctAnswer.length() == 0) {
+            Toast.makeText(this, "One Option Must Be Selected", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        progressDialog.setTitle("Save Question");
+        progressDialog.setMessage("Please wait while the question is added to the test...");
+        progressDialog.show();
+
+        Question questionObj = new Question(testId, questionId, question, option1, option2, option3, option4, correctAnswer);
+        firestore.collection(url)
+                .document(questionId)
+                .set(questionObj)
+                .addOnCompleteListener(res -> {
+                    if (res.isSuccessful()) {
+                        Toast.makeText(this, "Question updated successfully to the test.", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(this, "Could not add the question to the test. Please try again.", Toast.LENGTH_SHORT).show();
+                    }
+                    progressDialog.dismiss();
+                });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                this.onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/imad/quickclassquiz/recyclerview/QuestionListAdapter.java
+++ app/src/main/java/com/imad/quickclassquiz/recyclerview/QuestionListAdapter.java
@@ -2,6 +2,7 @@ package com.imad.quickclassquiz.recyclerview;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -17,6 +18,7 @@ import android.widget.Toast;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.imad.quickclassquiz.R;
+import com.imad.quickclassquiz.activities.EditQuestion;
 import com.imad.quickclassquiz.dataModel.Question;
 
 import java.util.ArrayList;
@@ -93,7 +95,10 @@ public class QuestionListAdapter extends RecyclerView.Adapter<QuestionListAdapte
 
         holder.editButton.setOnClickListener(v -> {
             // TODO Add code to edit a question
-            Snackbar.make(rootView, "Add code to edit this question", Snackbar.LENGTH_SHORT).show();
+           // Snackbar.make(rootView, "Add code to edit this question", Snackbar.LENGTH_SHORT).show();
+            Intent intent = new Intent(mContext, EditQuestion.class);
+            intent.putExtra("Question",list.get(position));
+            mContext.startActivity(intent);
         });
     }
 
